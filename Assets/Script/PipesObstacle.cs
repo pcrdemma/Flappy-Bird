f24b370580ed9ea7466094263d8abfa6fadcbf6d
Emma PICARD <emma.picard@ynov.com>
2024-05-13 17:03:14 +0200

    ajout d'obstacle

---
+++ Assets/Script/PipesObstacle.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipesObstacle : MonoBehaviour
+{
+    public float speed = 5f;
+    private float endScreen;
+
+    private void Start()
+    {
+        endScreen = Camera.main.ScreenToWorldPoint(Vector3.zero).x -1f;
+    }
+   
+
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (transform.position.x < endScreen)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
