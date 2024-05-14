6c2f16aa406af2983078c41fd922e4cebd8c03a2
Emma PICARD <emma.picard@ynov.com>
2024-05-14 10:39:40 +0200

    changement animation flappy bird

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,9 +4,8 @@ public class PlayerController : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
 
-    public Sprite[] sprites;
-
-    private int currentSprite;
+    [SerializeField]
+    private Animator animator;
 
     private Vector3 direction;
 
@@ -19,12 +18,6 @@ public class PlayerController : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>(); 
     }
 
-
-    private void Start()
-    {
-        InvokeRepeating(nameof(AnimateSprite), 0.15f, 0.15f);
-    }
-
     private void OnEnable()
     {
         Vector3 position = transform.position;
@@ -35,37 +28,30 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
             direction = Vector3.up * strength;
+            AnimateSprite();  // Appeler l'animation lorsqu'une entrée est détectée
         }
 
         direction.y += gravity * Time.deltaTime;
         transform.position += direction * Time.deltaTime;
 
-        
     }
 
-    private void AnimateSprite()
+    private void  AnimateSprite()
         //animation de l'oiseau
     {
-        currentSprite ++;
-
-        if(currentSprite >= sprites.Length)
-        {
-            currentSprite = 0;
-        }
-        spriteRenderer.sprite = sprites[currentSprite];
-
+        animator.SetTrigger("OnClick");
     }
 
     private  void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Obstacle")
+        if (other.gameObject.CompareTag("Obstacle"))
         {
             FindObjectOfType<GameManager>().GameOver();
         }
-        else if(other.gameObject.tag == "Score")
+        else if (other.gameObject.CompareTag("Score"))
         {
             FindObjectOfType<GameManager>().IncreaseScore();
         }
