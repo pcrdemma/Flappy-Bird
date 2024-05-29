d7feb8c5eaa7d472d9c4773fdceea417583f6365
Emma PICARD <emma.picard@ynov.com>
2024-05-29 15:35:12 +0200

    debut du panel pause

--- Assets/Script/PausePanel.cs
+++ Assets/Script/PausePanel.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PausePanel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] GameObject pausePanel;
+
+    public void Pause ()
     {
-        
+        pausePanel.SetActive(true);
+    }
+    public void Home()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void Play()
+    {
+        pausePanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Restart()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
