a96ce415785007cf0613982963caf535a7623110
Emma PICARD <emma.picard@ynov.com>
2024-05-30 23:32:29 +0200

    menu fait et fonctionnel

--- Assets/Script/PausePanel.cs
+++ Assets/Script/PausePanel.cs
@@ -11,18 +11,22 @@ public class PausePanel : MonoBehaviour
     public void Pause ()
     {
         pausePanel.SetActive(true);
+        Time.timeScale = 0f;
     }
     public void Home()
     {
         SceneManager.LoadScene("MainMenu");
+        Time.timeScale = 1f;
     }
     public void Play()
     {
         pausePanel.SetActive(false);
+        Time.timeScale = 1f;
     }
 
     public void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1f;
     }
 }
